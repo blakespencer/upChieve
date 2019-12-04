a39b3fd3159ccea121d316dc5f42240e1b602e43
Blake Spencer <blake.spencer1@gmail.com>
2019-12-04 12:22:15 -0500

    [feature] initial commit with boilerplate express server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
