30ebc62ce9ca7df2a7250dba8520455049edc898
Blake Spencer <blake.spencer1@gmail.com>
2019-12-09 23:47:07 -0500

    [feature] can sort alphabetically and based on student size

--- client/index.js
+++ client/index.js
@@ -3,11 +3,41 @@ const vueApp = new Vue({
   data: {
     display: 'Hello World',
     results: [],
+    selected: '',
   },
   async mounted() {
     const res = await axios.get('/api/colleges');
-    console.log('res', res.data);
     const vm = this;
     vm.results = res.data;
+  },
+  methods: {
+    onChange(event) {
+      const vm = this;
+      const results = vm.results;
+      if (event.target.value === 'alphabet') {
+        const sorted = this.sortAlpha(results);
+        vm.results = sorted;
+      } else {
+        vm.results = this.sortSize(results);
+      }
+    },
+    sortAlpha(results) {
+      const sorted = results.sort((a, b) => {
+        if (a['school.name'] > b['school.name']) {
+          return 1;
+        }
+        if (b['school.name'] > a['school.name']) {
+          return -1;
+        }
+        return 0;
+      });
+      return sorted;
+    },
+    sortSize(results) {
+      const sorted = results.sort(
+        (a, b) => b['latest.student.size'] - a['latest.student.size']
+      );
+      return sorted;
+    },
   },
 });
